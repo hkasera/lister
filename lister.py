bb596e9e0aeb36350d621b4867cc646f796c8067
Harshita Kasera <hkasera5@gmail.com>
2015-12-16 17:00:14 +0530

    Added option to show only directory structure

--- lister.py
+++ lister.py
@@ -4,6 +4,7 @@ import os
 
 path = os.getcwd()
 storeInFile = False
+showFiles = True
 filepath = "structure.txt"
 f = ""
 
@@ -20,21 +21,26 @@ def out(str):
 def list_files(startpath):
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
+        indent =  ' ' * 4 * (level)
         out('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            out('{}{}'.format(subindent, f))
+        if showFiles:    	
+        	subindent = ' ' * 4 * (level + 1)
+        	for f in files:
+        		out('{}{}'.format(subindent, f))
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', help='Path of the folder (Default current folder)', type=str,required=False)
 parser.add_argument('--o', help='Output file to store the result', type=str,required=False)
+parser.add_argument('--onlyDir', help='Output file to store the result', type=bool,required=False)
 
 args = parser.parse_args()
 
 if args.path:
 	path = args.path
+
+if args.onlyDir:
+	showFiles = False
 
 if args.o:
 	storeInFile = True
